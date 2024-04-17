a4c41f88f12d37abf2b81ef2b711d61aeddc9340
JanChocyk <janischocyk@gmail.com>
2024-04-17 21:18:11 +0200

    Update projects: opimalization, prepare_data section, docstrings

--- app/main.py
+++ app/main.py
@@ -1,3 +1,32 @@
+"""
+Name: KonstytuRAG App (main.py)
+
+Description:
+An application that implements a graphical chat interface in the browser. The user can chat
+with a chatbot whose task is to answer questions about the Polish constitution.
+
+Author: Jan Chocyk
+
+Version: 1.0
+
+Setup:
+- Install all required libraries from the requirements.txt file.
+- Create a .env file and place it in the project's main folder.
+- Sign up for Pinecone and create a vector index (details in README.md), copy the API key.
+- Sign up for the OpenAI service and copy the API key.
+- Add variables named PINECONE_API_KEY and OPENAI_API_KEY to the .env file, set the keys as their values.
+- Run the following command in the main folder:
+    ```python prepare_data\prepare_data.py``` - Windows
+    ```python3 prepare_data/prepare_data.py``` - Linux
+Running this script creates a vector database and may take a few minutes.
+
+Execution:
+- In the main folder, execute the command:
+    ```streamlit run app\main.py --server.port 8080``` Windows
+    ```streamlit run app/main.py --server.port 8080``` Linux
+After running, the default browser window will open with the application.
+"""
+
 import sys
 sys.path.append("..")
 
@@ -8,33 +37,46 @@ import time
 import logging
 from dotenv import load_dotenv
 
-from konstytuRAG.app.tool import konstytuRAG, customize_chat_history, test_prompt
+from konstytuRAG.app.tool import konstytuRAG
 
 
 def stream_generate(response: str):
+    """
+    Description: A generator simulating the chatbot's thinking process, returning each word of the response every 0.05 seconds.
+
+    Parameters:
+    - response (str): The chatbot's response to be displayed in the chat window.
+
+    Returns:
+    (str): A single word from the response.
+    """
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
 
 
 def main():
-    logging.basicConfig(filename=f'app/logs/chat_msgs.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    """
+    Description: The main function that constructs the chat interface from Streamlit components, initializes the RAG, 
+    and handles the exchange of messages between the user and the chatbot.
+    """
+    logging.basicConfig(filename=f'app/logs/chat.log', format='%(asctime)s %(message)s', level=logging.WARNING)
 
     load_dotenv()
     chat = konstytuRAG()
     chat.init_rag_chain()
 
     st.title('KonstytuRAG')
-    st.markdown(body='Poznaj chat-bot, którego celem jest udzielić odpowiedzi na pytania dotyczące treści konstytucji Polski.')
-    st.markdown(body='Jeżeli chcesz uzyskać optymalne odpowiedzi od asystenta, to:')
-    st.markdown(body='- zadawaj pojedyncze, konkretne pytania, dotyczące treści konstytucji')
-    st.markdown(body='- dopytuj jeżeli asystent odpowiedział niedokładnie')
-    st.markdown(body='- nie wysyłaj wiadomości powitalnych, tylko od razu zadawaj pytania')
+    instruction = '''Poznaj chat-bot, którego celem jest udzielić odpowiedzi na pytania dotyczące treści konstytucji Polski.\n
+        Jeżeli chcesz uzyskać optymalne odpowiedzi od asystenta, to:\n
+        - zadawaj pojedyncze, konkretne pytania, dotyczące treści konstytucji\n
+        - dopytuj jeżeli asystent odpowiedział niedokładnie\n
+        - nie wysyłaj wiadomości powitalnych, tylko od razu zadawaj pytania'''
+    st.markdown(body=instruction, )
 
     if "buffer_memory" not in st.session_state:
         st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
 
-    # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
@@ -48,9 +90,7 @@ def main():
         with st.chat_message(message['role']):
             st.markdown(message['content'])
 
-    # Accept user input
     if prompt := st.chat_input("Co byś chciał/chciała się dowiedzieć?"):
-        # Add user message to chat history
         st.session_state.messages.append({"role": "human", "content": prompt})
         logging.info(f'Human: {prompt}')
         # Display user message in chat message container
@@ -58,13 +98,16 @@ def main():
             st.markdown(prompt)
 
         # Display assistant response in chat message container
-        with st.chat_message("assistant"):            
-            answer, source = chat.get_answer(prompt, st.session_state.buffer_memory.load_memory_variables({})['history'])
+        with st.chat_message("assistant"):
+            try:
+                answer, source = chat.get_answer(prompt, st.session_state.buffer_memory.load_memory_variables({})['history'])
+            except Exception as e:
+                logging.error(f'Error: {e}')
             response = st.write_stream(stream_generate(answer))
             st.write(source)
 
         # Add assistant response to chat history
-        answer_to_write = answer + '\n' + source 
+        answer_to_write = f'{answer}\n\n{source}' 
         st.session_state.messages.append({"role": "ai", "content": answer_to_write})
         st.session_state.buffer_memory.save_context({'input': prompt}, {'output': answer})
         logging.info(f'AI: {response}')
---
+++ prepare_data/prepare_data.py
@@ -0,0 +1,66 @@
+import sys
+sys.path.append("..")
+
+from dotenv import load_dotenv
+import re
+from langchain.docstore.document import Document
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+
+def get_chunks(text, word):
+    reg = fr'\b[A-Z]{word[1:]}\b'
+    pattern = re.compile(reg)
+    modified_text = re.sub(pattern, f'<split>{word}', text)
+    chunks_list = modified_text.split(sep='<split>')
+
+    return chunks_list
+
+def main():
+    load_dotenv()
+    try:
+        with open('prepare_data/data/konstytucjaRP.txt', 'r', encoding='utf-8') as stream:
+            text = stream.read()
+        print('Dane wczytane z pliku.')
+    except Exception:
+        print('Błąd odczytu.')
+
+    text = text.replace('\xa0', ' ')
+    text = text.replace('\n', ' ')
+
+    chapter_list = get_chunks(text, 'Rozdział')
+
+    docs = []
+    pattern_chap = r'Rozdział\s+[IVXLCDM]+'
+    pattern_art = r'Art\.\s+\d+\.'
+
+    introduction = chapter_list[0]
+    docs.append(Document(page_content=introduction, metadata={'source': 'Wstęp i preambuła'}))
+    chapter_list.pop(0)
+
+    for chapter in chapter_list:
+        matched_chap = re.findall(pattern_chap, chapter)[0]
+
+        chapter = chapter.replace(matched_chap, '')
+        art_list = get_chunks(chapter, 'Art')
+        for art in art_list:
+            matched_art = re.findall(pattern_art, art)
+
+            if len(matched_art) != 0:
+                art = art.replace(matched_art[0], '')
+                metadata = {'source': f'{matched_chap}, {matched_art[0]}'}
+                doc = Document(page_content=art, metadata=metadata)
+                docs.append(doc)
+
+    model_roberta = 'sdadas/mmlw-roberta-large'
+    embeddings = SentenceTransformerEmbeddings(model_name=model_roberta)
+
+    index_name = 'konstytucja'
+    PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f'Błąd: {e}')
